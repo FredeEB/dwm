6b0b9b8f1d14d578e9512eb92775b6d6a7c4651c
fredeb <frederikbraendstrup@gmail.com>
2020-07-27 21:24:50 +0200

    updated colors

--- dwm.c
+++ dwm.c
@@ -282,15 +282,15 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Victor Mono Nerd Font:size=13" };
 static const char dmenufont[]       = "Victor Mono Nerd Font:size=13";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray1[]       = "#282936";
+static const char col_gray2[]       = "#bd93f9";
+static const char col_gray3[]       = "#f8f8f2";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_magenta[]     = "#ff79c6";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
+	[SchemeSel]  = { col_gray4, col_gray2, col_gray2  },
 };
 
 /* tagging */
