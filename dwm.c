ab809feb98e860a5872cf604562eb9db6abb459a
fredeb <frederikbraendstrup@gmail.com>
2020-08-04 00:54:53 +0200

    removed unused vars

--- dwm.c
+++ dwm.c
@@ -289,12 +289,10 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Victor Mono Nerd Font:size=13" };
-static const char dmenufont[]       = "Victor Mono Nerd Font:size=13";
 static const char col_gray1[]       = "#282936";
 static const char col_gray2[]       = "#bd93f9";
 static const char col_gray3[]       = "#f8f8f2";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_magenta[]     = "#ff79c6";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
