3c4e06b216932f54eca1dfc463e9d0ecc8212b13
Frede Braendstrup <frederikbraendstrup@gmail.com>
2021-12-09 22:25:24 +0100

    removed unused buttons

--- dwm.c
+++ dwm.c
@@ -360,17 +360,9 @@ static Key keys[] = {
 /* click can be ClkTagBar, ClkStatusText, ClkWinTitle,
  * ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
-        /* click                event mask      button          function argument
-         */
-        {ClkWinTitle, 0, Button2, zoom, {0}},
-        {ClkStatusText, 0, Button2, spawn, {.v = termcmd}},
         {ClkClientWin, MODKEY, Button1, movemouse, {0}},
         {ClkClientWin, MODKEY, Button2, togglefloating, {0}},
         {ClkClientWin, MODKEY, Button3, resizemouse, {0}},
-        {ClkTagBar, 0, Button1, comboview, {0}},
-        {ClkTagBar, 0, Button3, toggleview, {0}},
-        {ClkTagBar, MODKEY, Button1, combotag, {0}},
-        {ClkTagBar, MODKEY, Button3, toggletag, {0}},
 };
 // --------------------------------- CONFIG END --------------------------
 
